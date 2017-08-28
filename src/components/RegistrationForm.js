c0cd2a13ff2b8e3c0d49a50841e26c85f71f29c8
yuralala8 <yc1435@nyu.edu>
2017-08-28 09:59:43 -0400

    Done.

---
+++ src/components/RegistrationForm.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class RegistrationForm extends React.Component {
+	render() {
+		return (
+			<form>
+				<input type="text" />
+				<input type="password" />
+				<button type="submit" />
+			</form>
+		);
+	}
+}
+
+export default RegistrationForm; 
---
+++ src/components/Webpage.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import FillerText from './FillerText';
+
+
+class Webpage extends React.Component {
+	render() {
+		return (
+			<div>
+			<title>`The world's coolest webpage`</title>
+			<FillerText />
+			<FillerText />
+			</div>
+			)
+	}
+}
+
+export default Webpage;
